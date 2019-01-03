18c8ad9a4342485da0f44d92741ee060c013242c
Dennis Iruguthu <dennisiruguthu@gmail.com>
2019-01-03 20:13:26 +0300

    learn class

---
+++ learn_class_2.py
@@ -0,0 +1,22 @@
+##employee profile with first name, last, pay, email
+##
+##generate an email address form the first and last name and @company.com suffix
+##generate the full name as a method
+
+
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first=first
+        self.last=last
+        self.pay=pay
+        self.email= first.lower()+'.'+last.lower()+'@company.com'
+
+    def fullname(self):
+        return self.first+' '+self.last
+
+emp_1=Employee('Pat', 'Nicoles', 40000)
+emp_2=Employee('Daisy','Sandrea',42000)
+
+print(emp_1.email)
+print(emp_2.fullname())
