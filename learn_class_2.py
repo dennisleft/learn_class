576014636b3daff0a66dc46961f62ac81598abfb
Dennis Iruguthu <dennisiruguthu@gmail.com>
2019-01-04 18:59:29 +0300

    count number of employees

--- learn_class_2.py
+++ learn_class_2.py
@@ -8,6 +8,7 @@ class Employee:
 
     ##class variable
     pay_raise = 1
+    number_of_employees = 0
 
 
     def __init__(self, first, last, pay):   ##instance variables
@@ -15,11 +16,17 @@ class Employee:
         self.last=last
         self.pay=pay
         self.email= first.lower()+'.'+last.lower()+'@company.com'
+        Employee.number_of_employees +=1
+        ##to my understanding, we use Employee.number_of employees because
+        ##to get the number of employees, we count the instances of the class
 
     def fullname(self):
         return self.first+' '+self.last
     def raise_p(self):
         return int(self.pay*self.pay_raise)
+
+print(Employee.number_of_employees)
+
 
 emp_1=Employee('Pat', 'Nicoles', 40000)
 emp_2=Employee('Daisy','Sandrea',45000)
@@ -29,6 +36,7 @@ print(emp_1.email)
 print(emp_2.fullname())
 emp_3.pay_raise=1.2
 print(emp_3.raise_p())
+print(Employee.number_of_employees)
 
 
 
