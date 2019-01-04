7ee670fd0df296f4f7ebba3fc2422c14456bba64
Dennis Iruguthu <dennisiruguthu@gmail.com>
2019-01-04 18:37:32 +0300

    added class variable

--- learn_class_2.py
+++ learn_class_2.py
@@ -6,7 +6,11 @@
 
 class Employee:
 
-    def __init__(self, first, last, pay):
+    ##class variable
+    pay_raise = 1
+
+
+    def __init__(self, first, last, pay):   ##instance variables
         self.first=first
         self.last=last
         self.pay=pay
@@ -14,6 +18,8 @@ class Employee:
 
     def fullname(self):
         return self.first+' '+self.last
+    def raise_p(self):
+        return int(self.pay*self.pay_raise)
 
 emp_1=Employee('Pat', 'Nicoles', 40000)
 emp_2=Employee('Daisy','Sandrea',45000)
@@ -21,3 +27,11 @@ emp_3=Employee('Kist','Klone',30000)
 
 print(emp_1.email)
 print(emp_2.fullname())
+emp_3.pay_raise=1.2
+print(emp_3.raise_p())
+
+
+
+
+
+
