3405824e8b5d768fa57dce8b86005ce7b9aa1f1d
Dennis Iruguthu <dennisiruguthu@gmail.com>
2019-01-07 13:19:13 +0300

    added class method as constructor and static method for day of wee

--- learn_class_2.py
+++ learn_class_2.py
@@ -2,7 +2,7 @@
 ##
 ##generate an email address form the first and last name and @company.com suffix
 ##generate the full name as a method
-
+import datetime ##for static method
 
 class Employee:
 
@@ -31,6 +31,27 @@ class Employee:
     def set_pay_raise(cls, percent):    #cls is conventional, just like self
         cls.pay_raise = percent
 
+    ##as an alternative constructor
+    ##if variables are gvn all together with a colon between, we can do this
+    @classmethod
+    def from_new_emp(cls, new_emp):
+        first, last, pay = new_emp.split(':')
+        return cls(first,last,pay)
+
+    ##order in last,pay,first
+    @classmethod
+    def from_rearrange(cls, arrange):
+        first, last, pay= arrange[2], arrange[0], arrange[1]
+        return cls(first,last,pay)
+
+    ##static method
+    @staticmethod
+    def is_weekday(day):
+        if day.weekday()==5 or day.weekday==6:
+            return 'Weekend'
+        return 'Weekday'
+
+
 
 print(Employee.number_of_employees)
 
@@ -45,12 +66,26 @@ emp_3.pay_raise=1.2
 print(emp_3.raise_p())
 print(Employee.number_of_employees)
 
-#using the class method
+#using the class method - var
 Employee.set_pay_raise(1.1)
 print(emp_2.raise_p())
 
+##using classmethod as constructor
+new_emp_str= 'Kate:Rivers:37000'
+emp_4= Employee.from_new_emp(new_emp_str)
+print(emp_4.email)
+
+print(Employee.number_of_employees)
+
+##arrange last pay first    ->  forst last pay
+new_emp_arrange = ('Chilo',50000,'Dan')
+emp_5=Employee.from_rearrange(new_emp_arrange)
+print(emp_5.first)
 
 
+##static method
+my_date = datetime.date(2015,2,15)
+print(Employee.is_weekday(my_date))
 
 
 
